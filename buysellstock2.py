c3d54fa31bb1e9600899670f443e47a383aa00f0
xyzhangaa <xyzpku06@gmail.com>
2015-03-25 10:05:56 -0400

    Create buysellstock2.py

---
+++ buysellstock2.py
@@ -0,0 +1,13 @@
+###Say you have an array for which the ith element is the price of a given stock on day i.
+###Design an algorithm to find the maximum profit. You may complete as many transactions as 
+###you like (ie, buy one and sell one share of the stock multiple times). However, you may 
+###not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+def maxprofitstock(price):
+	maxprofit = 0
+	if len(price) <= 1:
+		return maxprofit
+	for i in range(len(price)-1):
+		if price[i+1] > price[i]:
+			maxprofit = maxprofit + price[i+1]-price[i]
+	return maxprofit
