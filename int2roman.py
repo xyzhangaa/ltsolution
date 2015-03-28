2fa00f505901ddb36f5f2040b01e5465a9bfcb85
xyzhangaa <xyzpku06@gmail.com>
2015-03-28 16:19:00 -0400

    Create int2roman.py

---
+++ int2roman.py
@@ -0,0 +1,18 @@
+###Given an integer, convert it to a roman numeral.
+###Input is guaranteed to be within the range from 1 to 3999.
+
+def int2roman(num):
+  result = ''
+  transTable = {1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M"}
+  for i in sorted(transTable.keys())[::-1]
+    while num > i and num != 0:
+      num = num-i
+      result += i
+    if num == 0:
+      break
+  simplyTable = {'DCCCC': 'CM', 'CCCC':'CD', 'LXXXX':'XC', 'XXXX':'XL', 'VIIII':'IV', 'IIII':'IV'}
+  simplyContent = ["DCCCC", "CCCC", "LXXXX", "XXXX", "VIIII", "IIII"]    
+  for i in simplyContent:
+    if i in result:
+      result = result.replace('i',simplyTable[i])
+  return result
