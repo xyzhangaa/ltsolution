32f7a03aacc770dd5bc952425af51bb282474394
xyzhangaa <xyzpku06@gmail.com>
2015-03-28 21:20:51 -0400

    Create RemoveElements.py

---
+++ RemoveElements.py
@@ -0,0 +1,11 @@
+###Given an array and a value, remove all instances of that value in place and return the new length.
+###The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+def RemoveElem(nums,elem):
+  front,back = 0,0
+  while front < len(nums):
+    if nums[front] != elem:
+      nums[back] = elem
+      back += 1
+    front += 1
+  return back
