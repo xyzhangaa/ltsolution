1e23e6b5134937de236048365f2548d27be615c7
xyzhangaa <xyzpku06@gmail.com>
2015-03-28 21:12:27 -0400

    Create swapnodeinpair.py

---
+++ swapnodeinpair.py
@@ -0,0 +1,15 @@
+###Given a linked list, swap every two adjacent nodes and return its head.
+    
+    def swapPairs(head):
+        if head == None or head.next == None:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        p = dummy
+        while p.next and p.next.next:
+            tmp = p.next.next
+            p.next.next = tmp.next
+            tmp.next = p.next
+            p.next = tmp
+            p = p.next.next
+        return dummy.next
