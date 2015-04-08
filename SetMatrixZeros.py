d0882c5aba01f9ae41ebbd74e55cff660e367774
xyzhangaa <xyzpku06@gmail.com>
2015-04-08 19:47:52 -0400

    Create SetMatrixZeros.py

---
+++ SetMatrixZeros.py
@@ -0,0 +1,16 @@
+###Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+
+def setmatrixzeros(matrix):
+	nrow = len(matrix)
+	ncol = len(matrix[0])
+	row = [False for _ in range(nrow)]
+	col = [False for _ in range(ncol)]
+	for i in range(nrow):
+		for j in range(ncol):
+			if matrix[i][j]==0:
+				row[i] = True
+				col[j] = True
+	for i in range(nrow):
+		for j in range(ncol):
+			if row[i] or col[j]:
+				matrix[i][j] = 0
