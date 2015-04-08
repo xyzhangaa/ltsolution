2114f49bd68f58f5b529b01cdeb2a71584e14dec
xyzhangaa <xyzpku06@gmail.com>
2015-04-08 09:45:10 -0400

    Create InterleavingString.py

---
+++ InterleavingString.py
@@ -0,0 +1,23 @@
+###Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+###For example,
+###Given:
+###s1 = "aabcc",
+###s2 = "dbbca",
+
+###When s3 = "aadbbcbcac", return true.
+###When s3 = "aadbbbaccc", return false.
+
+def interleavestring(s1,s2,s3):
+	if len(s1)+len(s2) != len(s3):
+		return False
+	dp = [[False for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+	dp[0][0] = True
+	for i in range(1,len(s1)+1):
+		dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
+	for j in range(1,len(s2)+1):
+		dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+	for i in range(1,len(s1)+1):
+		for j in range(1,len(s2)+1):
+			dp[i][j] =(dp[i-1][j] and s1[i-1]==s3[i+j-1]) \
+				   or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+	return dp[len(s1)][len(s2)]
