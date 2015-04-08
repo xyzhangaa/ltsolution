4ed91b4db84f086ca77401dfae1b820a93eb134b
xyzhangaa <xyzpku06@gmail.com>
2015-04-08 16:29:44 -0400

    Create RestoreIP.py

---
+++ RestoreIP.py
@@ -0,0 +1,19 @@
+###Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+###For example:
+###Given "25525511135",
+###return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+
+def restoreIPaddress(s):
+	def dfs(s,sub,ips,ip):
+		if sub == 4:
+			if s == '':
+				ips.append(ip[1:])
+		for i in range(1,4):
+			if i <= len(s):
+				if int(s[:i]) <= 255:
+					dfs(s[i:],sub+1,ips,ip+'.'+s[:i])
+				if s[0] == '0':
+					break
+	ips = []
+	dfs(s,0,ips,'')
+	return ips
