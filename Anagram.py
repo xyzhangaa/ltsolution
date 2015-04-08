e508cad6cbaa5faff23d437b7f4db4609cbf76d3
xyzhangaa <xyzpku06@gmail.com>
2015-04-08 19:56:57 -0400

    Create Anagram.py

---
+++ Anagram.py
@@ -0,0 +1,16 @@
+###Given an array of strings, return all groups of strings that are anagrams.
+###Note: All inputs will be in lower-case.
+
+def anagram(strs):
+	htable = {}
+	for item in strs:
+		sortedword = ''.join(sorted(item))
+		if sortedword not in htable:
+			htable[sortedword] = [item]
+		else:
+			htable[sortedword] += [item]
+	result = []
+	for item in htable:
+		if len(htable[item]) >= 2:
+			result.append(htable[item])
+	return result
