8f79aea3e612f0939a461c3c51ce61a205fad321
xyzhangaa <xyzpku06@gmail.com>
2015-04-09 10:22:07 -0400

    Create RotateImage.py

---
+++ RotateImage.py
@@ -0,0 +1,17 @@
+###You are given an n x n 2D matrix representing an image.
+###Rotate the image by 90 degrees (clockwise).
+###Follow up:
+###Could you do this in-place?
+
+
+###invert and then reverse rows
+def rotateimage(matrix):
+	n = len(matrix)
+	for i in range(n):
+		for j in range(i+1,n):
+			temp = matrix[i][j]
+			matrix[i][j] = matrix[j][i]
+			matrix[j][i] = temp
+	for i in range(n):
+		matrix[i].reverse
+	return matrix
