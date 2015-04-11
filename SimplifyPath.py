f5c60cded3e1771f9f1af63d55cbba4a018a7a35
xyzhangaa <xyzpku06@gmail.com>
2015-04-11 15:14:10 -0400

    Create SimplifyPath.py

---
+++ SimplifyPath.py
@@ -0,0 +1,50 @@
+###Given an absolute path for a file (Unix-style), simplify it.
+
+###For example,
+###path = "/home/", => "/home"
+###path = "/a/./b/../../c/", => "/c"
+
+###Corner Cases:
+###Did you consider the case where path = "/../"? In this case, you should return "/".
+###Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/". 
+######In this case, you should ignore redundant slashes and return "/home/foo".
+
+###Python string manipulation
+def SimplifyPath(s):
+	s = s.split('/')
+	curr = '/'
+	for i in s:
+		if i == '..':
+			if curr != '/':
+				curr = '/'.join(curr.split('/')[:-1])
+				if curr == '':
+					curr = '/'
+		elif i != '.' and i !='':
+			if curr != '/':
+				curr += '/'+i
+			else:
+				curr += i
+	return curr
+
+### No string manipulation
+def SimplifyPathTwo(s):
+	stack = []
+	i = 0
+	result = ''
+	while i < len(s):
+		end = i+1
+		while end < len(s) and s[end] != '/':
+			end += 1
+		sub = s[i+1:end]
+		if len(sub) > 0:
+			if sub == '..':
+				if stack != []:
+					stack.pop()
+			elif sub != '.':
+				stack.append(sub)
+		i = end
+	if stack == []:
+		return '/'
+	for i in stack:
+		result += '/'+i
+	return result
