e4c9ce6bf85f91c2f97fbbae0df3df4d29071959
xyzhangaa <xyzpku06@gmail.com>
2015-04-11 12:02:06 -0400

    Create RemoveDupSortedList.py

---
+++ RemoveDupSortedList.py
@@ -0,0 +1,21 @@
+###Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+###For example,
+###Given 1->1->2, return 1->2.
+###Given 1->1->2->3->3, return 1->2->3.
+
+class ListNode:
+	def __init__(self,x):
+		self.val = x
+		self.next = None
+
+def RemoveDupSortedLinkedList(self,head):
+	if head == None or head.next == None:
+		return head
+	p = head
+	while p.next:
+		if p.val == p.next.val:
+			p.next = p.next.next
+		else:
+			p=p.next
+	return head
