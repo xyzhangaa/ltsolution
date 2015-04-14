53f117144ff1e00a5b902063efd42c0b6a1f774a
xyzhangaa <xyzpku06@gmail.com>
2015-04-14 11:11:26 -0400

    Create SingleNum.py

---
+++ SingleNum.py
@@ -0,0 +1,7 @@
+###Given an array of integers, every element appears twice except for one. Find that single one.
+
+def SingleNum(A):
+	res = A[0]
+	for i in range(1,len(A)):
+		res = res ^ A[i]
+	return res
