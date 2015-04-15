36c90858637be3f8ae205e36454effe96baaa8f3
xyzhangaa <xyzpku06@gmail.com>
2015-04-15 11:59:06 -0400

    Create BTLevelOrder.py

---
+++ BTLevelOrder.py
@@ -0,0 +1,14 @@
+###Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+def preorder(root,level,res):
+	if root:
+		if len(res) < level +1:
+			res.append([])
+		res[level].append(root.val)
+		preorder(root.left,level+1,res)
+		preorder(root.right,level+1,res)
+		
+def BTLevelOrder(root):
+	res = []
+	preorder(root,0,res)
+	return res
