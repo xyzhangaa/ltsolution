f03777300fc1a3832acf3ace5a72440f9436f2b6
xyzhangaa <xyzpku06@gmail.com>
2015-04-15 10:48:42 -0400

    Create PopulateNodesTwo.py

---
+++ PopulateNodesTwo.py
@@ -0,0 +1,76 @@
+###Follow up for problem "Populating Next Right Pointers in Each Node".
+
+###What if the given tree could be any binary tree? Would your previous solution still work?
+
+###Note:
+
+###You may only use constant extra space.
+
+class TreeLinkedNode:
+  def __int__(self,x):
+    self.val = x
+    self.left= None
+    self.right = None
+    self.next = None
+
+###step by step, similar to PopulateNodes
+def PopulateNodesTwo(root):
+	if root:
+		if root.left and root.right:
+			root.left.next = root.right
+			temp = root.next
+			while temp:
+				if temp.left:
+					root.right.next = temp.left
+					break
+				if temp.right:
+					root.right.next = temp.right
+					break
+				temp = temp.next
+		elif root.left:
+			temp = root.next
+			while temp:
+				if temp.left:
+					root.left.next = temp.left
+					break
+				if temp.right:
+					root.right.next = temp.right
+					break
+				temp = temp.next
+		elif root.right:
+			temp = root.next
+			while temp:
+				if temp.left:
+					root.right.next = temp.left
+					break
+				if temp.right:
+					root.right.next = temp.right
+					break
+				temp = temp.next
+		PopulateNodesTwo(root.right)
+		PopulateNodesTwo(root.left)
+
+
+### More concise way
+def PopulateNodesTwo(root):
+	if root:
+		p = root
+		q = None
+		nextNode = None
+		while p:
+			if p.left:
+				if q:
+					q.next = p.left
+				q = p.left
+				if nextNode == None:
+					nextNode = q
+			if p.right:
+				if q:
+					q.next = p.right
+				q = p.right
+				if nextNode == None:
+					nextNode = q
+			p = p.next
+		PopulateNodesTwo(nextNode)
+
+
