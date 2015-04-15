87c99d18f04af4df20a256151dda9467cf415347
xyzhangaa <xyzpku06@gmail.com>
2015-04-15 11:27:30 -0400

    Create SumRoot2Leaf.py

---
+++ SumRoot2Leaf.py
@@ -0,0 +1,16 @@
+###Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+###An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+###Find the total sum of all root-to-leaf numbers.
+
+def sum(root,preSum):
+	if root == None:
+		return 0
+	preSum = preSum*10+root.val
+	if root.left == None and root.right == None:
+		return preSum
+	return sum(root.left,preSum)+sum(root.right,preSum)
+	
+def SumRoot2Leadf(root):
+	return sum(root,0)
