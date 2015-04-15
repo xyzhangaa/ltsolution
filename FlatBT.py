a4fde3efc4cf4bfd66df202869f629005df462e4
xyzhangaa <xyzpku06@gmail.com>
2015-04-15 13:15:53 -0400

    Create FlatBT.py

---
+++ FlatBT.py
@@ -0,0 +1,16 @@
+###Given a binary tree, flatten it to a linked list in-place.
+
+def FlatBT(root):
+	if root == None:
+		return
+	FlatBT(root.left)
+	FlatBT(root.right)
+	p = root
+	if p.left == None:
+		return
+	p = p.left
+	while p.right:
+		p = p.right
+	p.right = root.right
+	root.right = root.left
+	root.left = None
