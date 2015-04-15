da184ccae9bd00bb8612e2dc78e3d7934c60c5a2
xyzhangaa <xyzpku06@gmail.com>
2015-04-15 11:10:12 -0400

    Create CopyList.py

---
+++ CopyList.py
@@ -0,0 +1,30 @@
+###A linked list is given such that each node contains an additional random pointer 
+###which could point to any node in the list or null.
+
+###Return a deep copy of the list.
+
+def CopyList(head):
+	if head == None:
+		return None
+	temp = head
+	while temp:
+		newNode = RandomListNode(temp.label)
+		newNode.next= temp.next
+		temp.next = newNode
+		temp = temp.next.next
+	temp = head
+	while temp:
+		if temp.random:
+			temp.next.random = temp.random.next
+		temp = temp.next.next
+	newhead = head.next
+	pold = head
+	pnew = newhead
+	while pnew.next:
+		pold.next = pnew.next
+		pold = pold.next
+		pnew.next = pold.next
+		pnew = pnew.next
+	pold.next= None
+	pnew.next= None
+	return newhead
