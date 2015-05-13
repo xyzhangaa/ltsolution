2f8cad9d007ad5873b1acaf8970d5e65c1ba378c
xyzhangaa <xyzpku06@gmail.com>
2015-05-13 11:51:34 -0400

    Update ReverseNodesKGroup.py

--- ReverseNodesKGroup.py
+++ ReverseNodesKGroup.py
@@ -12,35 +12,35 @@
 
 ###For k = 3, you should return: 3->2->1->4->5
 
-class ListNode:
-	def __init__(self,x):
-		self.val = x
-		self.next = None
+###class ListNode:
+###	def __init__(self,x):
+###		self.val = x
+###		self.next = None
+class Solution:
+	def reverse(self,start,end):
+		newhead = ListNode(0)
+		newhead.next = start
+		while newhead.next!= end:
+			temp  = start.next
+			start.next = temp.next
+			temp.next = newhead.next
+			newhead.next = temp
+		return [end,start]
 
-def reverse(start,end):
-	newhead = ListNode(0)
-	newhead.next = start
-	while newhead.next!= end:
-		temp  = start.next
-		start.next = temp.next
-		temp.next = newhead.next
-		newhead.next = temp
-	return [end,start]
-
-def reverseKgroup(head,k):
-	if head == None:
-		return None
-	nhead = ListNode(0)
-	nhead.next = head
-	start = nhead
-	while start.next:
-		end = start
-		for i in range(k-1):
-			end = end.next
-			if end.next == None:
-				return nhead.next
-		result = reverse(start.next,end.next)
-		start.next = result[0]
-		start = result[1]
-	return nhead.next
+	def reverseKgroup(head,k):
+		if head == None:
+			return None
+		nhead = ListNode(0)
+		nhead.next = head
+		start = nhead
+		while start.next:
+			end = start
+			for i in range(k-1):
+				end = end.next
+				if end.next == None:
+					return nhead.next
+			result = self.reverse(start.next,end.next)
+			start.next = result[0]
+			start = result[1]
+		return nhead.next
 
