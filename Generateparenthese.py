e7516dc22654fab040eac344a68ce2f156c7ce2b
xyzhangaa <xyzpku06@gmail.com>
2015-05-16 16:12:59 -0400

    Update Generateparenthese.py

--- Generateparenthese.py
+++ Generateparenthese.py
@@ -1,10 +1,20 @@
 ###Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
-def _generateParenthesis(n, m):
-        if n == 0:      return [")"*m]
-        elif n == m:    return ["("+i for i in _generateParenthesis(n-1, m)]
-        else:           return ["("+i for i in _generateParenthesis(n-1, m)] + \
-                               [")"+i for i in _generateParenthesis(n, m-1)]
-                               
-def generateParenthesis(n):
-	    return _generateParenthesis(n, n)
+class Solution:
+    # @param {integer} n
+    # @return {string[]}
+    def helper(self, l, r, item, res):
+        if r < l:
+            return
+        if l == 0 and r ==0:
+            res.append(item)
+        if l > 0:
+            self.helper(l-1,r,item+'(',res)
+        if r > 0:
+            self.helper(l,r-1,item+')',res)
+    def generateParenthesis(self, n):
+        if n == 0:
+            return []
+        res = []
+        self.helper(n,n,'',res)
+        return res
