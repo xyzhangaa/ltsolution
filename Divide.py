94a95de10b81f0542ba6facb9d9108b6a6a01fc6
xyzhangaa <xyzpku06@gmail.com>
2015-05-16 19:39:04 -0400

    Update Divide.py

--- Divide.py
+++ Divide.py
@@ -1,23 +1,38 @@
 
 ###Divide two integers without using multiplication, division and mod operator.
 
-def divide(dividend,divisor):
-	if (dividend < 0 and divisor > 0) or (dividend>0 and divisor <0):
-		if abs(dividend) < abs(divisor):
-			return 0
-	total =0
-	count = 0
-	res = 0
-	a = abs(dividend)
-	b = abs(divisor)
-	while a >= b:
-		sum = b
-		count = 1
-		while sum+sum <= a:
-			sum += sum
-			count += count
-		a -= sum
-		res += count
-	if (dividend < 0 and divisor > 0) or (dividend>0 and divisor <0):
-		res = 0- res
-	return res
+class Solution:
+    # @param {integer} dividend
+    # @param {integer} divisor
+    # @return {integer}
+    def divide(self, dividend, divisor):
+        if divisor == 0:
+            return False
+        if dividend == 0:
+            return 0
+        if (dividend < 0 and divisor >0) or (dividend > 0 and divisor < 0):
+            if abs(dividend) < abs(divisor):
+                return 0
+        sum = 0
+        count = 0
+        res = 0
+        max_int = 2147483647; min_int=-2147483648
+        a=abs(divisor); b=abs(dividend)
+        while b>=a:
+            sum = a
+            count =1
+            while sum*2 <= b:
+                sum += sum
+                count += count
+            b -= sum
+            res += count
+        if (dividend < 0 and divisor >0) or (dividend > 0 and divisor < 0):
+            if res*(-1) < min_int:
+                return min_int
+            else:
+                return res*(-1)
+        else:
+            if res > max_int:
+                return max_int
+            else:
+                return res
