214e55e358cdd544520624c80388512061fb1ada
xyzhangaa <xyzpku06@gmail.com>
2015-05-16 19:30:42 -0400

    Update Mergesortedlinkedlist.py

--- Mergesortedlinkedlist.py
+++ Mergesortedlinkedlist.py
@@ -1,9 +1,9 @@
 ###Merge two sorted linked lists and return it as a new list. The new list should be 
 ###made by splicing together the nodes of the first two lists.
 
-class ListNode(self,x):
-	self.val = x
-	self.next = None
+###class ListNode(self,x):
+###	self.val = x
+###	self.next = None
 
 def mergetwosortedlinkedlist(self,l1,l2):
 	if l1 == None:
@@ -16,11 +16,10 @@ def mergetwosortedlinkedlist(self,l1,l2):
 		if l1.val <= l2.val:
 			temp.next = l1
 			l1 = l1.next
-			temp = temp.next
 		else:
 			temp.next = l2
 			l2 = l2.next
-			temp = temp.next
+		temp = temp.next
 	if l1 == None:
 		temp.next = l2
 	else:
