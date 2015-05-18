66fb68e4ac61419e2050ec17dd83767e170aa6af
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 11:16:01 -0400

    Update Validparenthese.py

--- Validparenthese.py
+++ Validparenthese.py
@@ -1,5 +1,12 @@
-###Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-###The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+# Time:  O(n)
+# Space: O(n)
+#
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+# determine if the input string is valid.
+# 
+# The brackets must close in the correct order, "()" and "()[]{}" 
+# are all valid but "(]" and "([)]" are not.
+#
 
 def validparenthese(s):
   matchparenthese = {'(' : ')', '[':']', '{':'}'}
@@ -7,10 +14,13 @@ def validparenthese(s):
   for element in s:
     if element == '(' or element =='[' or element == '{':
       stack.append(element)
-    elif element == '(' or element ==']' or element == '}':
-      if stack.pop() != element or len(stack) == 0:
+    elif element == ')' or element ==']' or element == '}':
+      if len(stack) == 0:
+        return False
+        break
+      elif stack.pop() != matchparenthese[element]:
         return False
         break
       else:
-        pass
+        continue
   return len(stack) == 0
