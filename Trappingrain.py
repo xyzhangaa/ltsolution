245ebdd6dbfc3251e6ca31ebfff36d15e4ba6fc4
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 15:22:15 -0400

    Update Trappingrain.py

--- Trappingrain.py
+++ Trappingrain.py
@@ -1,7 +1,7 @@
 ###Given n non-negative integers representing an elevation map where the width of each bar is 1, 
 ###compute how much water it is able to trap after raining.
 # time O(n)
-#space O(1)
+#space O(n)
 
     def trap(A):
         leftmosthigh = [0]*len(A)
@@ -41,6 +41,32 @@ class Solution2:
             stack.append([i, A[i]])
             
         return result
-    
+
+#time O(n)
+#space O(1)
+
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    def trap(self, A):
+        result = 0
+        top = 0
+        for i in xrange(len(A)):
+            if A[top] < A[i]:
+                top = i
+        
+        second_top = 0
+        for i in xrange(top):
+            if A[second_top] < A[i]:
+                second_top = i
+            result += A[second_top] - A[i]
+            
+        second_top = len(A) - 1
+        for i in reversed(xrange(top, len(A))):
+            if A[second_top] < A[i]:
+                second_top = i
+            result += A[second_top] - A[i]
+            
+        return result    
 if __name__ == "__main__":
     print Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
