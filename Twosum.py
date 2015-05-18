c18b1d9edcb94041bcc436f407db2e3e92ca11d6
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 00:59:39 -0400

    Update Twosum.py

--- Twosum.py
+++ Twosum.py
@@ -15,3 +15,17 @@ def twosum(array,target):
     else:
       end -= 1
   
+#hash table solution
+#time O(n)
+#space O(n)
+class Solution:
+  def twosum(self,nums,target):
+    lookup = {}
+    for i,num in enumerate(nums):
+      if target - num in lookup:
+        return (lookup[target-num]+1,i+1)
+      else:
+        lookup[num] = i
+
+if __name__ == '__main__':
+  print 'index1=%d, index2=%d' %Solution.twosum((2,7,11,15),9)
