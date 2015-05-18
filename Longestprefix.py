b326a4d941088dd1a6b429e07b9aa4dccef877a8
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 09:29:55 -0400

    Update Longestprefix.py

--- Longestprefix.py
+++ Longestprefix.py
@@ -1,3 +1,9 @@
+# Time:  O(n1 + n2 + ...)
+# Space: O(1)
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+
+
 def longestprefix(strs):
 	longestpre = strs[0]
 	for string in strs[1:]:
@@ -7,3 +13,6 @@ def longestprefix(strs):
 			i += 1
 		longestpre = min(longestpre,i)
 	return strs[0][:longestpre]
+
+if __name__ == "__main__":
+    print Solution().longestCommonPrefix(["hello", "heaven", "heavy"])
