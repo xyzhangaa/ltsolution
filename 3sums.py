236ae55cad8320a8b87236c81a21937f4ea4a9e0
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 10:51:03 -0400

    Update 3sums.py

--- 3sums.py
+++ 3sums.py
@@ -1,5 +1,19 @@
-###Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? 
-###Find all unique triplets in the array which gives the sum of zero.
+# Time:  O(n^2)
+# Space: O(1)
+#
+# Given an array S of n integers,
+# are there elements a, b, c in S such that a + b + c = 0?
+# Find all unique triplets in the array which gives the sum of zero.
+#
+# Note:
+# Elements in a triplet (a,b,c) must be in non-descending order. (ie, a <= b <= c)
+# The solution set must not contain duplicate triplets.
+#    For example, given array S = {-1 0 1 2 -1 -4},
+#
+#    A solution set is:
+#    (-1, 0, 1)
+#    (-1, -1, 2)
+#
 
 def 3sum(nums):
   nums.sort()
@@ -32,6 +46,8 @@ def 3sum(nums):
       i += 1
   return result
 
-      
+if __name__ == '__main__':
+    result = Solution().threeSum([-1, 0, 1, 2, -1, -4])
+    print result
     
     
