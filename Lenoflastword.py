41281ce22b4cecdf030c8024cd62f7d95655cab2
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 17:21:18 -0400

    Update Lenoflastword.py

--- Lenoflastword.py
+++ Lenoflastword.py
@@ -1,30 +1,34 @@
-###Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
-###return the length of last word in the string.
-###If the last word does not exist, return 0.
+# Time:  O(n)
+# Space: O(1)
+#
+# Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+# 
+# If the last word does not exist, return 0.
+# 
+# Note: A word is defined as a character sequence consists of non-space characters only.
+# 
+# For example, 
+# Given s = "Hello World",
+# return 5.
+#
 
-def lastword(s):
-  return len(s.split()[len(s.split())-1] if s.split() != [] else 0
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def lengthOfLastWord(self, s):
+        length = 0
+        for i in reversed(s):
+            if i == ' ':
+                if length:
+                    break
+            else:
+                length += 1
+        return length
 
-
-### if the special functions forbidden 
-
-def lastword(s):
-  i = len(s)-1
-  count = 0
-  while i >= 0:
-    if s[i] == ' ':
-      i -= 1
-    else:
-      break
-  if i == len(s):
-    return count
-  else:
-    while i >=0:
-      if s[i] != ' ':
-        count += 1
-        i -= 1
-      else:
-        break
-    return count
-      
-  
+# Time:  O(n)
+# Space: O(n)
+class Solution2:
+    # @param s, a string
+    # @return an integer
+    def lengthOfLastWord(self, s):
+        return len(s.strip().split(" ")[-1])
