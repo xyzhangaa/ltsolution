5e2f52df655a89aa0ba98fae0e98fd7eceb8b711
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 09:28:41 -0400

    Update Roman2Integer.py

--- Roman2Integer.py
+++ Roman2Integer.py
@@ -1,5 +1,10 @@
-###Given a roman numeral, convert it to an integer.
-###Input is guaranteed to be within the range from 1 to 3999.
+# Time:  O(n)
+# Space: O(1)
+#
+# Given a roman numeral, convert it to an integer.
+# 
+# Input is guaranteed to be within the xrange from 1 to 3999.
+#
 
 def romanToInt(s):
         transSingleTable = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
@@ -17,3 +22,7 @@ def romanToInt(s):
             print False
             break
         return result
+        
+if __name__ == "__main__":
+    print Solution().romanToInt("IIVX")
+    print Solution().romanToInt("MMMCMXCIX")
