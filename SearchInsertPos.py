8b053385b828f9130b5469ee37add88c335063a9
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 12:23:55 -0400

    Update SearchInsertPos.py

--- SearchInsertPos.py
+++ SearchInsertPos.py
@@ -1,6 +1,8 @@
 ###Given a sorted array and a target value, return the index if the target is found. 
 ###If not, return the index where it would be if it were inserted in order.
 ###You may assume no duplicates in the array.
+#time O(logn)
+#space O(1)
 
 def searchinsertpos(A,target):
   begin,end = 0, len(A)-1
@@ -16,3 +18,9 @@ def searchinsertpos(A,target):
       begin = mid+1
       insertpos=mid+1
   return insertpos
+
+if __name__ == "__main__":
+    print Solution().searchInsert([1, 3, 5, 6], 5)
+    print Solution().searchInsert([1, 3, 5, 6], 2)
+    print Solution().searchInsert([1, 3, 5, 6], 7)
+    print Solution().searchInsert([1, 3, 5, 6], 0)
