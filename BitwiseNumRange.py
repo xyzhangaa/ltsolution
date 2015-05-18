e449f45b5de565916f3c7715aa20a14509f8c95f
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 00:19:49 -0400

    Update BitwiseNumRange.py

--- BitwiseNumRange.py
+++ BitwiseNumRange.py
@@ -15,3 +15,11 @@ class Solution:
       i += 1
     return n&m >> i << i
     
+class Solution2:
+    def bitwisenumrange(self,m,n):
+       p =0
+       while m != n:
+         m >>= 1
+         n >>= 1
+         p += 1
+      return m << p
