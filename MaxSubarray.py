0ca73e87de40f522378994b8a81a59576d1c4286
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 15:59:48 -0400

    Update MaxSubarray.py

--- MaxSubarray.py
+++ MaxSubarray.py
@@ -3,24 +3,17 @@
 ###For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
 ###the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 
+#time O(n)
+#space O(1)
 
-
-
-def MaxSubarray(nums):
-	i = 0
-	while nums[i] <= 0:
-		i+=1
-	maxsum =nums[i]
-	total = nums[i]
-	begin,end = i,i+1
-	while i < len(nums):
-		total += nums[i]
-		if total <= 0:
-			begin = i+1
-			end = begin
-			total = 0
-		elif maxsum < total:
-			maxsum = total
-			end = i
-		i += 1
-	return begin,end,maxsum
+class Solution:
+	def maximumsubarray(self,A):
+		maxsum = -100000
+		curr = 0
+		for i in range(1,len(A)):
+			if curr < 0:
+				curr = 0
+			curr += A[i]
+			maxsum = max(maxsum,curr)
+		return maxsum
+			
