3c94dd56c141765e9f442ff81bb75a2ce4afa125
xyzhangaa <xyzpku06@gmail.com>
2015-05-17 22:07:35 -0400

    Update BSTIterator.py

--- BSTIterator.py
+++ BSTIterator.py
@@ -14,11 +14,16 @@ class TreeNode:
     self.right = None
 
 class BSTIterator:
+      # @param root, a binary search tree's root node
   def __init__(self,root):
     self.stack = []
     self.cur = root
+    
+        # @return a boolean, whether we have a next smallest number
   def hasNext(self):
     return self.stack or self.cur
+    
+        # @return an integer, the next smallest number
   def next(self):
     while self.cur:
       self.stack.append(self.cur)
