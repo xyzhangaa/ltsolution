1dea0a3686cc5d16ede58582e54dc61a1bbb7d5e
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 10:59:03 -0400

    Update 4sum.py

--- 4sum.py
+++ 4sum.py
@@ -1,6 +1,8 @@
 ###Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? 
 ###Find all unique quadruplets in the array which gives the sum of target.
-
+# Time:  O(n^2 * p)
+# Space: O(n^2 * p)
+#hash table solution
 def 4sum(S,target):
       result = []
       num.sort()
@@ -53,3 +55,26 @@ def 4sum(S,target):
                 num[firstIndex] == num[firstIndex-1]:
               firstIndex += 1
       return result
+
+# Time:  O(n^2 * p) ~ O(n^4)
+# Space: O(n^2)
+class Solution2:
+    # @return a list of lists of length 4, [[val1,val2,val3,val4]]
+    def fourSum(self, nums, target):
+        nums, result, lookup = sorted(nums), [], {}
+        for i in xrange(0, len(nums) - 1):
+            for j in xrange(i + 1, len(nums)): 
+                if nums[i] + nums[j] not in lookup:
+                    lookup[nums[i] + nums[j]] = []
+                lookup[nums[i] + nums[j]].append([i, j])
+
+        for i in lookup.keys():
+            if target - i in lookup:
+                for x in lookup[i]:
+                    for y in lookup[target -i]:
+                        [a, b], [c, d] = x, y
+                        if a is not c and a is not d and b is not c and b is not d:
+                            quad = sorted([nums[a], nums[b], nums[c], nums[d]])
+                            if quad not in result:
+                                result.append(quad)
+        return sorted(result)
