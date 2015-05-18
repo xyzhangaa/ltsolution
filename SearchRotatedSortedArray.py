cf66bc4781f80abf1ea9e45aae7ac032bf664988
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 12:10:21 -0400

    Update SearchRotatedSortedArray.py

--- SearchRotatedSortedArray.py
+++ SearchRotatedSortedArray.py
@@ -2,6 +2,8 @@
 ###(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
 ###You are given a target value to search. If found in the array return its index, otherwise return -1.
 ###You may assume no duplicate exists in the array.
+#time (logn)
+#space O(1)
 
 def _searchtargethelper(A,target,begin,end):
 	if begin > end:
@@ -28,3 +30,28 @@ def _searchtargethelper(A,target,begin,end):
 
 def searchtarget(A,target):
 	return _searchtargethelper(A,target,0,len(A)-1)
+
+
+#####
+class Solution2:
+	def search(self, A, target):
+        	low, high = 0, len(A)
+        
+        	while low < high:
+            	mid = low + (high - low) / 2
+            
+            	if A[mid] == target:
+                	return mid
+            
+            	if A[low] <= A[mid]:
+                	if A[low] <= target and target < A[mid]:
+                    		high = mid
+                	else:
+                    	low = mid + 1
+            	else:
+                	if A[mid] < target and target <= A[high - 1]:
+                    		low = mid + 1
+                	else:
+                    		high = mid
+                    
+        	return -1
