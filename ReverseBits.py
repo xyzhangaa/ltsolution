5a69a0233ab94522ab038e7bd7088f963122dfc7
xyzhangaa <xyzpku06@gmail.com>
2015-05-17 23:53:57 -0400

    Update ReverseBits.py

--- ReverseBits.py
+++ ReverseBits.py
@@ -21,3 +21,17 @@ class Solution:
       res |= n&1
       n >>= 1
       
+char tb[16] = {0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15};
+
+uint32_t reverseBits(uint32_t n) {
+        int curr = 0;
+        uint32_t ret = 0;
+        uint32_t msk = 0xF;
+        for(int i = 0; i < 8; i++) {
+            ret = ret << 4;
+            curr = msk&n;
+            ret |= tb[curr];
+            n = n >> 4;
+        }
+        return ret;
+}
