331e5969b9879bc6ba429541cf63d8a42dbabd44
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 15:46:42 -0400

    Update Permutation.py

--- Permutation.py
+++ Permutation.py
@@ -3,6 +3,8 @@
 ###For example,
 ###[1,2,3] have the following permutations:
 ###[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+## Time:  O(n!)
+# Space: O(n)
 
 def Permutation(num):
 	if len(num) == 0:
@@ -14,3 +16,24 @@ def Permutation(num):
 		for j in Permutation(num[:i]+num[i+1:]):
 			result.append([num[i]]+j)
 	return result
+
+class Solution:
+    # @param num, a list of integer
+    # @return a list of lists of integers
+    def permute(self, num):
+        result = []
+        used = [False] * len(num)
+        self.permuteRecu(result, used, [], num)
+        return result
+    
+    def permuteRecu(self, result, used, cur, num):
+        if len(cur) == len(num):
+            result.append(cur + [])
+            return
+        for i in xrange(len(num)):
+            if not used[i]:
+                used[i] = True
+                cur.append(num[i])
+                self.permuteRecu(result, used, cur, num)
+                cur.pop()
+                used[i] = False
