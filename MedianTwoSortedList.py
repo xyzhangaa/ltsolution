135fe4c621cb151e1bdcce8e075bc8b5da93f9c1
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 01:01:26 -0400

    Update MedianTwoSortedList.py

--- MedianTwoSortedList.py
+++ MedianTwoSortedList.py
@@ -1,7 +1,8 @@
 ###There are two sorted arrays A and B of size m and n respectively. 
 ###Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 
-
+# time O(log(m+n))
+# time O(1)
 
 def getKth(a,b,k):
 	if len(a) > len(b):
