b485a4a225b8bcac06077565ed7444a93fbd8327
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 10:58:33 -0400

    Update BTLevelOrderTwo.py

--- BTLevelOrderTwo.py
+++ BTLevelOrderTwo.py
@@ -1,6 +1,7 @@
 ###Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
-333(ie, from left to right, level by level from leaf to root).
+###(ie, from left to right, level by level from leaf to root).
 
+#O(n), O{n}
 def preorder(root,level,res):
 	if root:
 		if len(res) < level +1:
