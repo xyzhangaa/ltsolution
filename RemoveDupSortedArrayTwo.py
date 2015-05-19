9c47fa0f5da625cc26ef0fbc8d5a68d7543870ec
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 23:34:20 -0400

    Update RemoveDupSortedArrayTwo.py

--- RemoveDupSortedArrayTwo.py
+++ RemoveDupSortedArrayTwo.py
@@ -1,23 +1,31 @@
-###Follow up for "Remove Duplicates":
-###What if duplicates are allowed at most twice?
+# Time:  O(n)
+# Space: O(1)
+#
+# Follow up for "Remove Duplicates":
+# What if duplicates are allowed at most twice?
+# 
+# For example,
+# Given sorted array A = [1,1,1,2,2,3],
+# 
+# Your function should return length = 5, and A is now [1,1,2,2,3].
+#
 
-###For example,
-###Given sorted array A = [1,1,1,2,2,3],
+class Solution:
+    # @param a list of integers
+    # @return an integer
+    def removeDuplicates(self, A):
+        if not A:
+            return 0
+        
+        last, i, same = 0, 1, False
+        while i < len(A):
+            if A[last] != A[i] or not same:
+                same = A[last] == A[i]
+                last += 1
+                A[last] = A[i]
+            i += 1
+            
+        return last + 1
 
-###Your function should return length = 5, and A is now [1,1,2,2,3].
-
-
-def RemoveDupSortedArrayTwo(A):
-	if len(A) <= 2:
-		return A
-	prev = 1
-	curr = 2
-	while curr < len(A):
-		if A[curr] == A[prev] and A[curr] == A[prev-1]:
-			curr += 1
-		else:
-			prev += 1
-			A[prev] = A[curr]
-			curr += 1
-	del A[prev+1:]
-	return A, prev+1
+if __name__ == "__main__":
+    print Solution().removeDuplicates([1, 1, 1, 2, 2, 3])
