c319bd7de53f16e1cbe6e0bec2575666334ab2b1
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 11:49:21 -0400

    Update Distinctsub.py

--- Distinctsub.py
+++ Distinctsub.py
@@ -21,3 +21,22 @@ def distinctsub(s,t):
 			else:
 				dp[i][j] = dp[i-1][j]
 	return dp[len(s)][len(t)]
+
+# Time:  O(n^2)
+# Space: O(n)
+class Solution:
+    # @return an integer
+    def numDistinct(self, S, T):
+        ways = [0 for _ in xrange(len(T) + 1)]
+        ways[0] = 1
+        for S_char in S:
+            for j, T_char in reversed(list(enumerate(T))):
+                if S_char == T_char:
+                    ways[j + 1] += ways[j]
+        return ways[len(T)]
+        
+if __name__ == "__main__":
+    S = "rabbbit"
+    T = "rabbit"
+    result = Solution().numDistinct(S, T)
+    print result
