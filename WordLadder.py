2c6861419f460670e474002950ebd962d7d590d4
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 12:24:15 -0400

    Update WordLadder.py

--- WordLadder.py
+++ WordLadder.py
@@ -19,22 +19,33 @@
 ###All words have the same length.
 ###All words contain only lowercase alphabetic characters.
 
-def WordLadder(start,end,diction):
-	diction.append(end)
-	q = []
-	q.append((start,1))
-	while q:
-		curr = q.pop(0)
-		currword = curr[0]
-		count = curr[1]
-		if currword == end:
-			return count
-		for item in diction:
-			nondup = 0
-			for i in range(len(currword)):
-				if item[i] != currword[i]:
-					nondup += 1
-			if nondup == 1:
-				q.append((item,count+1))
-				diction.remove(item)
-	return 0
+#O(n*d), O(d)
+# BFS
+class Solution:
+    # @param start, a string
+    # @param end, a string
+    # @param dict, a set of string
+    # @return an integer
+    def ladderLength(self, start, end, dict):
+        distance, cur, visited = 0, [start], set([start])
+        dict.add(end)
+        
+        while cur:
+            next = []
+            for word in cur:
+                if word == end:
+                    return distance + 1
+                for i in xrange(len(word)):
+                    for j in 'abcdefghijklmnopqrstuvwxyz':
+                        candidate = word[:i] + j + word[i + 1:]
+                        if candidate not in visited and candidate in dict:
+                            next.append(candidate)
+                            visited.add(candidate)
+            distance += 1
+            cur = next
+        
+        return 0
+    
+if __name__ == "__main__":
+    print Solution().ladderLength("hit", "cog", set(["hot","dot","dog","lot","log"]))
+
