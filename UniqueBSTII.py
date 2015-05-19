3a009675a70a236d7c6f0450f078e60bd235afc0
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 10:36:06 -0400

    Update UniqueBSTII.py

--- UniqueBSTII.py
+++ UniqueBSTII.py
@@ -1,19 +1,21 @@
 ###Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
 ###List the Tree Nodes
 
+
+#O(4^n/n^(3/2)), O(4^n/n^(3/2))
 class TreeNode:
 	def __int__(self,x):
 		self.val=x
 		self.left = None
 		self.right = None
 		
-def dfs(start,end):
+def dfs(self, start,end):
 	if start > end:
 		return [None]
 	res = []
 	for rootval in range(start,end+1):
-		lefttree = dfs(start,rootval-1)
-		righttree = dfs(rootval+1,end)
+		lefttree = self.dfs(start,rootval-1)
+		righttree = self.dfs(rootval+1,end)
 		for i in lefttree:
 			for j in righttree:
 				root = TreeNode(rootval)
@@ -23,4 +25,4 @@ def dfs(start,end):
 		return res
 	
 def generateBTS(n):
-	return dfs(1,n)
+	return self.dfs(1,n)
