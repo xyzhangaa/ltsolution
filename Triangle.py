b7aebcd2ff927db512f2c3b9bf8ee9f6c58f4a1d
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 12:01:37 -0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,6 +1,7 @@
 ###Given a triangle, find the minimum path sum from top to bottom. 
 ###Each step you may move to adjacent numbers on the row below.
 
+#O(n*m),O(n)
 def Triangle(array):
 	if len(array) == 0:
 		return 0
