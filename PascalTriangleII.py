f0e1377ee7fdde808cbcb2ae06f07cae1a7092d9
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 11:57:59 -0400

    Update PascalTriangleII.py

--- PascalTriangleII.py
+++ PascalTriangleII.py
@@ -1,15 +1,32 @@
-def PascalTriangle(nRows):
-	if nRows == 0:
-		return []
-	if nRows == 1:
-		return [[1]]
-	if nRows == 2:
-		return [[1],[1,1]]
-	if nRows > 2:
-		list = [[] for _ in range(nRows+1)]
-		for i in range(0,nRows):
-			list[i] = [1 for j in range(i+1)]
-		for i in range(2,nRows+1):
-			for j in range(1,i):
-				list[i][j]=list[i-1][j-1]+list[i-1][j]
-		return list[nRows]
+# Time:  O(n^2)
+# Space: O(n)
+#
+# Given an index k, return the kth row of the Pascal's triangle.
+# 
+# For example, given k = 3,
+# Return [1,3,3,1].
+# 
+# Note:
+# Could you optimize your algorithm to use only O(k) extra space?
+#
+
+class Solution:
+    # @return a list of integers
+    def getRow(self, rowIndex):
+        result = [1]
+        for i in range(1, rowIndex + 1):
+            result = [1] + [result[j - 1] + result[j] for j in range(1, i)] + [1]
+        return result
+
+class Solution2:
+    # @return a list of integers
+    def getRow(self, rowIndex):
+        result = [0] * (rowIndex + 1)
+        for i in xrange(rowIndex + 1):
+            old = result[0] = 1
+            for j in xrange(1, i + 1):
+                old, result[j] = result[j], old + result[j]
+        return result
+
+if __name__ == "__main__":
+    print Solution().getRow(3)
