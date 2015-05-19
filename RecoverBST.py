b87e94eb6cf68ab7eaf6eebc43f545df29681393
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 10:49:58 -0400

    Update RecoverBST.py

--- RecoverBST.py
+++ RecoverBST.py
@@ -2,19 +2,47 @@
 
 ###Recover the tree without changing its structure.
 
-def FindTwoNodes(root):
-	if root:
-		FindTwoNodes(root.left)
-		if prev and prev.val > root.val:
-			n2 = root
-			if n1 == None:
-				n1 = prev
-		prev = root
-		FindTwoNodes(root.right)
-		
-def RecoverBST(root):
-	n1 = n2 = None
-	prev = None
-	FindTwoNodes(root)
-	n1.val,n2.val = n2.val,n1.val
-	return root
+#O(n), O(1)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {void} Do not return anything, modify root in-place instead.
+            
+    def recoverTree(self, root):
+        return self.morristraversal(root)
+    
+    def morristraversal(self,root):
+        if root is None:
+            return 
+        broken = [None,None]
+        pre, cur = None,root
+        while cur:
+            if cur.left is None:
+                self.detectbroken(broken,pre,cur)
+                pre =cur
+                cur = cur.right
+            else:
+                node= cur.left
+                while node.right and node.right != cur:
+                    node = node.right
+                if node.right is None:
+                    node.right = cur
+                    cur = cur.left
+                else:
+                    self.detectbroken(broken,pre,cur)
+                    node.right  = None
+                    pre = cur
+                    cur = cur.right
+        broken[0].val,broken[1].val = broken[1].val,broken[0].val
+
+    def detectbroken(self,broken,pre,cur):
+        if pre and pre.val > cur.val:
+            if broken[0] is None:
+                broken[0] = pre
+            broken[1] =cur
