eceeec25936437a43dcf38a147b5f0d8e6cc316c
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 21:33:59 -0400

    Update UniquePathTwo.py

--- UniquePathTwo.py
+++ UniquePathTwo.py
@@ -4,6 +4,8 @@
 
 ###An obstacle and empty space is marked as 1 and 0 respectively in the grid.
 
+#time O(m*n)
+#space O(m*n)
 class Solution:
     # @param {integer[][]} obstacleGrid
     # @return {integer}
@@ -32,3 +34,39 @@ class Solution:
                  else:
                      dp[i][j] = dp[i-1][j]+dp[i][j-1]
          return dp[m-1][n-1]
+
+class Solution:
+    # @param obstacleGrid, a list of lists of integers
+    # @return an integer
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        ways = [0] * n
+        
+        if obstacleGrid[0][0] == 0:
+            ways[0] = 1
+            
+        for j in xrange(1, n):
+            if obstacleGrid[0][j] == 1:
+                ways[j] = 0
+            else:
+                ways[j] = ways[j - 1]
+        
+        for i in xrange(1, m):
+            if obstacleGrid[i][0] == 1:
+                ways[0] = 0
+                
+            for j in xrange(1, n):
+                if obstacleGrid[i][j] == 1:
+                    ways[j] = 0
+                else:
+                    ways[j] += ways[j - 1]
+        
+        return ways[n - 1]
+
+if __name__ == "__main__":
+    obstacleGrid = [
+                      [0,0,0],
+                      [0,1,0],
+                      [0,0,0]
+                   ]
+    print Solution().uniquePathsWithObstacles(obstacleGrid)
