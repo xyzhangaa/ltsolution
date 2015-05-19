c4e3e2d628e44fb983c5eea6eb119f0024a6e1f5
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 10:57:27 -0400

    Update MaxDepthBinaryTree.py

--- MaxDepthBinaryTree.py
+++ MaxDepthBinaryTree.py
@@ -2,15 +2,29 @@
 
 ###The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
-#O(n), O(1)
-class TreeNode:
-	def __int__(self,x):
-		self.val = x
-		self.left = None
-		self.right = None
+#O(n), O(n)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-def MaxDepthBinaryTree(self,root):
-	if root == None:
-		return None
-	else:
-		return max(MaxDepthBinaryTree(self,root.left), MaxDepthBinaryTree(self,root,right)) +1
+class Solution:
+    # @param {integer[]} preorder
+    # @param {integer[]} inorder
+    # @return {TreeNode}
+    def buildTree(self, preorder, inorder):
+        lookup = {}
+        for i, num in enumerate(inorder):
+            lookup[num] = i
+        return self.buildTreeRecu(lookup,preorder,inorder,0,0,len(inorder))
+        
+    def buildTreeRecu(self,lookup,preorder,inorder,pre_start,in_start,in_end):
+        if in_start == in_end:
+            return None
+        node = TreeNode(preorder[pre_start])
+        i = lookup[preorder[pre_start]]
+        node.left = self.buildTreeRecu(lookup,preorder,inorder,pre_start+1,in_start,i)
+        node.right = self.buildTreeRecu(lookup,preorder,inorder,pre_start+1+i-in_start,i+1,in_end)
+        return node
