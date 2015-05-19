45de3edfed23f530702e65fe23920ce57855607a
xyzhangaa <xyzpku06@gmail.com>
2015-05-19 11:44:51 -0400

    Update ConstructBTInPostorder.py

--- ConstructBTInPostorder.py
+++ ConstructBTInPostorder.py
@@ -1,13 +1,31 @@
 ###Given inorder and postorder traversal of a tree, construct the binary tree.
 
-###Note:You may assume that duplicates do not exist in the tree.
-def ConstructBTInPostoder(inorder,postorder):
-	if len(inorder) ==0 or len(postorder) ==0:
-		return None
-	if len(inorder) == 1:
-		return TreeNode(inorder[0])
-	root = TreeNode(postorder[-1])
-	index = inorder.index(postorder[-1])
-	root.left= ConstructBTInPostorder(inorder[:index],postorder[:index])
-	root.right=ConstructBTInPostorder(inorder[index+1:],postorder[index:-1])
-	return root
+# Time:  O(n)
+# Space: O(n)
+#
+class Solution:
+    # @param inorder, a list of integers
+    # @param postorder, a list of integers
+    # @return a tree node
+    def buildTree(self, inorder, postorder):
+        lookup = {}
+        for i, num in enumerate(inorder):
+            lookup[num] = i
+        return self.buildTreeRecu(lookup, postorder, inorder, len(postorder), 0, len(inorder))
+    
+    def buildTreeRecu(self, lookup, postorder, inorder, post_end, in_start, in_end):
+        if in_start == in_end:
+            return None
+        node = TreeNode(postorder[post_end - 1])
+        i = lookup[postorder[post_end - 1]]
+        node.left = self.buildTreeRecu(lookup, postorder, inorder, post_end - 1 - (in_end - i - 1), in_start, i)
+        node.right = self.buildTreeRecu(lookup, postorder, inorder, post_end - 1, i + 1, in_end)
+        return node
+
+if __name__ ==  "__main__":
+    inorder = [2, 1, 3]
+    postorder = [2, 3, 1]
+    result = Solution().buildTree(inorder, postorder)
+    print result.val
+    print result.left.val
+    print result.right.val
