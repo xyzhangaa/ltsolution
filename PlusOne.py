865aca3df1b35c5f6a953eee1aacab45a3b67d14
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 22:16:01 -0400

    Update PlusOne.py

--- PlusOne.py
+++ PlusOne.py
@@ -1,14 +1,15 @@
 ###Given a non-negative number represented as an array of digits, plus one to the number.
 ###The digits are stored such that the most significant digit is at the head of the list.
-
+#time O(n)
+#space O(1)
 def PlusOne(num):
 	carry = 1
 	for i in range(len(num)-1,-1,-1):
 		if (num[i]+carry) >= 10:
+			carry = (num[i]+carry)/10
 			num[i] = (num[i]+carry)%10
-			carry = 1
 		else:
-			num[i] = num[i]+carry
+			num[i] += carry
 			carry = 0
 	if carry == 1:
 		num.insert(0,1)
