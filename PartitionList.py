36f6258c1f54df73c2a58eaca9cb96e4b24707d8
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 23:52:00 -0400

    Update PartitionList.py

--- PartitionList.py
+++ PartitionList.py
@@ -1,28 +1,52 @@
-###Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
-###You should preserve the original relative order of the nodes in each of the two partitions.
+# Time:  O(n)
+# Space: O(1)
+#
+# Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+# 
+# You should preserve the original relative order of the nodes in each of the two partitions.
+# 
+# For example,
+# Given 1->4->3->2->5->2 and x = 3,
+# return 1->2->2->4->3->5.
+#
 
+# Definition for singly-linked list.
 class ListNode:
-  def __int__(self,x):
-    self.val = x
-    self.next = None
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+    
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, repr(self.next))
 
-def PartitionList(head,a):
-  head1 = ListNode(0)
-  head2 = ListNode(0)
-  temp = head
-  phead1 = head1
-  phead2 = head2
-  while temp:
-    if temp.val < a:
-      phead1.next = temp
-      temp = temp.next
-      phead1 = phead1.next
-      phead1.next = None
-    else:
-      phead2.next = temp
-      temp = temp.next
-      phead2 = phead2.next
-      phead2.next = None
-  phead1.next=head2.next
-  head=head1.next
-  return head
+class Solution:
+    # @param head, a ListNode
+    # @param x, an integer
+    # @return a ListNode
+    def partition(self, head, x):
+        dummySmaller, dummyGreater = ListNode(-1), ListNode(-1)
+        smaller, greater = dummySmaller, dummyGreater
+        
+        while head:
+            if head.val < x:
+                smaller.next = head
+                smaller = smaller.next
+            else:
+                greater.next = head
+                greater = greater.next
+            head = head.next
+            
+        smaller.next = dummyGreater.next
+        greater.next = None
+        
+        return dummySmaller.next
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(4)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(2)
+    head.next.next.next.next = ListNode(5)
+    head.next.next.next.next.next = ListNode(2)
+    print Solution().partition(head, 3)
