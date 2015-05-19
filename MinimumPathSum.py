ea11a76a6ad01ea0d082cf0a00aa872339d20c24
xyzhangaa <xyzpku06@gmail.com>
2015-05-18 22:08:56 -0400

    Update MinimumPathSum.py

--- MinimumPathSum.py
+++ MinimumPathSum.py
@@ -15,4 +15,27 @@ def minpathsum(grid):
     for j in range(1,col):
       A[i][j] = min(grid[i-1,j],grid[i,j-1])+grid[i][j]
   return A[row-1][col-1]
-  
+ 
+#time O(m*n)
+#space O(m+n)
+class Solution:
+    # @param grid, a list of lists of integers
+    # @return an integer
+    def minPathSum(self, grid):
+        sum = list(grid[0])
+        for j in xrange(1, len(grid[0])):
+            sum[j] = sum[j - 1] + grid[0][j]
+
+        for i in xrange(1, len(grid)):
+            sum[0] += grid[i][0]
+            for j in xrange(1, len(grid[0])):
+                sum[j] = min(sum[j - 1], sum[j]) + grid[i][j]
+                
+        return sum[-1]
+    
+if __name__ == "__main__":
+    print Solution().minPathSum([[0,1]
+                                ,[1,0]])
+    print Solution().minPathSum([[1,3,1]
+                                ,[1,5,1]
+                                ,[4,2,1]])
