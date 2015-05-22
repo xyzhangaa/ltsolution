0215ec5d1e53301fbd3756d0b69a4060a92b2d85
xyzhangaa <xyzpku06@gmail.com>
2015-05-22 15:13:23 -0400

    Update AddTwoNum.py

--- AddTwoNum.py
+++ AddTwoNum.py
@@ -4,30 +4,37 @@
 ###You are given two linked lists representing two non-negative numbers. 
 ###The digits are stored in reverse order and each of their nodes contain 
 ###a single digit. Add the two numbers and return it as a linked list.
-class ListNode:
-  def __int__(self,x):
-    self.val = x
-    self.next=None
+class Solution:
+    # @param {ListNode} l1
+    # @param {ListNode} l2
+    # @return {ListNode}
+    def addTwoNumbers(self, l1, l2):
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        dummy = ListNode(0)
+        curr = dummy; carry = 0
+        while l1 and l2:
+            val = l1.val+l2.val+carry
+            curr.next = ListNode(val%10)
+            carry = val/10
+            curr=curr.next
+            l1=l1.next
+            l2=l2.next
+        while l1:
+            val = l1.val+carry
+            curr.next = ListNode(val%10)
+            carry = val/10
+            curr=curr.next
+            l1=l1.next
+        while l2:
+            val = l2.val+carry
+            curr.next = ListNode(val%10)
+            carry = val/10
+            curr=curr.next
+            l2=l2.next
+        if carry == 1:
+            curr.next = ListNode(carry)
+        return dummy.next
   
-def addtwonum(a,b):
-  return _addtwonum(a,b,0)
-
-def _addtwonum(a,b,carry):
-  value = carry
-  if b == None:
-    a,b = b,a
-  if a == None and b == None:
-    if value = 0:
-      return None
-    else:
-      return ListNode(value)
-    elif a == None:
-      value += b.val
-      tempNode = ListNode(value%10)
-      tempNode.next = _addtwonum(None,b.next,value/10)
-      return tempNode
-    else:
-      value += a.val+b.val
-      tempNode = ListNode(value%10)
-      tempNode.next = _addtwonum(a.next,b.next,value/10)
-      return tempNode
