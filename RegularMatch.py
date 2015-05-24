2875ae6168cc2daaab57ec5a67721a00b10c7eee
xyzhangaa <xyzpku06@gmail.com>
2015-05-24 09:46:21 -0400

    Update RegularMatch.py

--- RegularMatch.py
+++ RegularMatch.py
@@ -151,6 +151,29 @@ class Solution4:
                 s = s[1:]
             return self.isMatch(s, p[2:])
 
+class Solution:
+    # @return a boolean
+    def isMatch(self, s, p):
+        previousRow = [True]
+        for i in range(0, len(p)):
+            if p[i]=='*':
+                previousRow.append(previousRow[i-1])
+            else:
+                previousRow.append(False)
+                
+        for letter in range(0,len(s)):
+            actualRow = [False];
+            for i in range(0, len(p)):
+                if p[i]=='*':
+                    temp = actualRow[i-1] or (previousRow[i+1] and (p[i-1]==s[letter] or p[i-1]=='.'))
+                elif p[i] == '.':
+                    temp = previousRow[i]
+                else:
+                    temp = previousRow[i] and p[i]==s[letter] 
+                actualRow.append(temp)
+            previousRow = actualRow
+        return previousRow[len(p)]
+                
 if __name__ == "__main__":
     print Solution3().isMatch("abab", "a*b*")
     print Solution().isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c")
