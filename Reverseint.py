1cf99ae408ce54c9e8b0de949ff75d0b7e34d1bc
xyzhangaa <xyzpku06@gmail.com>
2015-06-15 10:51:45 -0400

    Update Reverseint.py

--- Reverseint.py
+++ Reverseint.py
@@ -1,24 +1,3 @@
-def reverseint(num):
-	if num == 0:
-		print(num)
-	elif num < 0:
-		newnumstr = str(num)[:-len(str(num)):-1]
-		i=0
-		while newnumstr[i] == '0':
-			i += 1
-		result = '-'+newnumstr[i:len(newnumstr)]
-	else:
-		newnumstr = str(num)[::-1]
-		i=0
-		while newnumstr[i] == '0':
-			i+= 1
-		result = newnumstr[i:len(newnumstr)]
-	if -2147483648L <= int(result) <= -147483647L:
-	  print result
-	else:
-	  print 'Overflow'
-	  
-	  
 # Time: O(logn)
 # Space: O(1)
 #
@@ -44,6 +23,7 @@ def reverseint(num):
 class Solution:
     # @return an integer
     def reverse(self, x):
+    	max_int = 2147483648
         ans = 0
         if x >= 0:
             while x:
